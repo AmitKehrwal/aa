5616ddfb7cabba9f5a5d8c2443f4516522fa00dc
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-10 15:37:08 +0530

    Update Pyppetter.py

--- Pyppetter.py
+++ Pyppetter.py
@@ -1,51 +1,16 @@
-import asyncio
-from playwright.async_api import async_playwright
-import indian_names
+import subprocess
 
-async def start(thread_name, wait_time, meetingcode, passcode):
-    user = indian_names.get_full_name()
-    print(f"{thread_name} started!")
+# Install curl
+subprocess.run(['sudo', 'apt', 'install', 'curl'])
 
-    async with async_playwright() as p:
-        # Use Firefox browser with specified executable path
-        browser = await p.firefox.launch(headless=True)
-        context = await browser.new_context()
-        page = await context.new_page()
-        await page.goto(f'https://zoom.us/wc/join/{meetingcode}', timeout=200000)
+# Download the Brave browser keyring
+subprocess.run(['sudo', 'curl', '-fsSLo', '/usr/share/keyrings/brave-browser-archive-keyring.gpg', 'https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg'])
 
-        try:
-            await page.click('//button[@id="onetrust-accept-btn-handler"]', timeout=5000)
-        except Exception as e:
-            pass
+# Add Brave browser repository to sources.list.d
+subprocess.run(['echo', 'deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main', '|', 'sudo', 'tee', '/etc/apt/sources.list.d/brave-browser-release.list'])
 
-        try:
-            await page.click('//button[@id="wc_agree1"]', timeout=5000)
-        except Exception as e:
-            pass
+# Update package list
+subprocess.run(['sudo', 'apt', 'update'])
 
-        try:
-            await page.wait_for_selector('input[type="text"]', timeout=200000)
-            await page.fill('input[type="text"]', user)
-            await page.fill('input[type="password"]', passcode)
-            join_button = await page.wait_for_selector('button.preview-join-button', timeout=200000)
-            await join_button.click()
-        except Exception as e:
-            pass
-
-        try:
-            query = '//button[text()="Join Audio by Computer"]'
-            await asyncio.sleep(13)
-            mic_button_locator = await page.wait_for_selector(query, timeout=350000)
-            await asyncio.sleep(10)
-            await mic_button_locator.evaluate_handle('node => node.click()')
-            print(f"{thread_name} mic aayenge.")
-        except Exception as e:
-            print(f"{thread_name} mic nahe aayenge. ", e)
-
-        print(f"{thread_name} sleep for {wait_time} seconds ...")
-        while wait_time > 0:
-            await asyncio.sleep(1)
-            wait_time -= 1
-        print(f"{thread_name} ended!")
-
-        await browser.close()
+# Install Brave browser
+subprocess.run(['sudo', 'apt', 'install', 'brave-browser'])
