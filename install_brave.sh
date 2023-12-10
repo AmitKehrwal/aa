13aab784cc428b526478195eca5661709a9c2e9a
AmitKehrwal <144267580+AmitKehrwal@users.noreply.github.com>
2023-12-10 15:48:20 +0530

    Update install_brave.sh

--- install_brave.sh
+++ install_brave.sh
@@ -1,16 +1,16 @@
-#!/bin/bash     
+#!/bin/bash
 
 # Install curl
-sudo apt install -y curl
+sudo apt-get install -y curl
 
 # Download the Brave browser keyring
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://$
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
 # Add Brave browser repository to sources.list.d
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] htt$
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 
 # Update package list
-sudo apt update
+sudo apt-get update
 
 # Install Brave browser
-sudo apt install -y brave-browser
+sudo apt-get install -y brave-browser
